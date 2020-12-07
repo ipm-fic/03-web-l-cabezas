c0bd25740c79f7e15e2a5354ce510d719668b80b
laura <l.cabezas@udc.es>
2020-12-07 21:00:25 +0100

    error servidor y mejora visual

--- src/getData.js
+++ src/getData.js
@@ -20,13 +20,26 @@ async function getPokemon(name = "pikachu") {
     var param = document.getElementById("pokeInput").value.toLowerCase();
     name =param
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    if(!res.ok){
+        const pokemonEl = document.createElement("div");
+        pokemonEl.classList.add("pokemon");
+        pokemonEl.innerHTML = `
+        <div class="error">
+            <h2> Pokémon no encontrado o no existe </h2>
+            <img src="img/pikachuTriste.gif" width="500">        
+        </div>`
+        pokemonContainer.appendChild(pokemonEl);
+        return
+    }
+
+
     const pokemon = await res.json();
 
     const pokemonEl = document.createElement("div");
     pokemonEl.classList.add("pokemon");
 
     pokemonEl.innerHTML = `
-
+       
     <div class="info">
       <img src="https://pokeres.bastionbot.org/images/pokemon/${
         pokemon.id
@@ -34,15 +47,15 @@ async function getPokemon(name = "pikachu") {
     <h2>${pokemon.name}</h2>
     </div>
 
-    <div class="stats">
+    <div class="stats" >
       ${pokemon.stats
         .map((stat) => {
-            return `<p>${stat.stat.name}: ${stat.base_stat}</p>`;
+            return `<p >${stat.stat.name}: ${stat.base_stat}</p>`;
         })
         .join("")}
     </div>
 
-    <div class="abilities">
+    <div class="abilities" style="padding-bottom: -2% ">
     ${pokemon.abilities
         .map((ability) => {
             return `<p> ability: ${ability.ability.name}</p>`;
