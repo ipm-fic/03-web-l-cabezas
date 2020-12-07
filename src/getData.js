fdc50bdf775737da4fb245e30dbf87494759e5ae
laura <l.cabezas@udc.es>
2020-12-07 11:06:31 +0100

    petición y respuesta servidor

---
+++ src/getData.js
@@ -0,0 +1,63 @@
+
+// grab the things we need ----------
+const pokemonContainer = document.querySelector(".pokemon-container");
+const formEl = document.querySelector("form");
+const inputEl = document.querySelector("input[type=text]");
+
+console.log(inputEl);
+
+// listen for user events -------------
+formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    pokemonContainer.innerHTML = "";
+    getPokemon(inputEl.value);
+});
+
+
+
+// define our functions/actions ------------
+async function getPokemon(name = "pikachu") {
+    var param = document.getElementById("pokeInput").value.toLowerCase();
+    name =param
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const pokemon = await res.json();
+
+    const pokemonEl = document.createElement("div");
+    pokemonEl.classList.add("pokemon");
+
+    pokemonEl.innerHTML = `
+
+    <div class="info">
+      <img src="https://pokeres.bastionbot.org/images/pokemon/${
+        pokemon.id
+    }.png" width="200">
+    <h2>${pokemon.name}</h2>
+    </div>
+
+    <div class="stats">
+      ${pokemon.stats
+        .map((stat) => {
+            return `<p>${stat.stat.name}: ${stat.base_stat}</p>`;
+        })
+        .join("")}
+    </div>
+
+    <div class="abilities">
+    ${pokemon.abilities
+        .map((ability) => {
+            return `<p> ability: ${ability.ability.name}</p>`;
+        })
+        .join("")}
+    <div>  
+    
+  `;
+
+    pokemonContainer.appendChild(pokemonEl);
+}
+
+// run things ----------------
+getPokemon();
+
+
+
+
